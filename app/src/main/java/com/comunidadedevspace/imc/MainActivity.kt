e05fa4a96490ed96ebc34b4a4ccb9a2eca4852b6
thierrystofel <playsthierry@gmail.com>
2025-01-29 15:10:58 -0300

    Basics UI and Main Activity implemented

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,28 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // access the weight and high field
+        val edtPeso = findViewById<TextInputEditText>(R.id.edtPeso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edtAltura)
+
+        // access the button
+        val btnCalcular = findViewById<Button>(R.id.btnCalcular)
+        btnCalcular.setOnClickListener() {
+            // get the weight and high value
+            val peso = edtPeso.text.toString().toDouble()
+            val altura = edtAltura.text.toString().toDouble()
+
+
+
+        }
+
     }
 }
